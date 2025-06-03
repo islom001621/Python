45ba0bce8d66d492d841275db1171e81caf00938
islom001621 <dustmurdovislom@gmail.com>
2025-06-03 15:03:59 +0500

    Create 1-dars.py

---
+++ lesson-1/homework/1-dars.py
@@ -0,0 +1,38 @@
+# task 1
+side=int(input('Enter the lenght of side of the square:8'))
+Perimetr=4*side
+Area=side**2
+print('Kvatratni Perimetri:',Perimetr)
+print('Kvadratni Yuzi:',Area)
+print(Perimetr)
+print(Area)
+
+
+
+#task 2
+Radius=int(input('Enter the radius of circle:7'))
+Diameter=2*Radius
+print('Diametr:',Diameter)
+
+# task 3
+
+a=10
+b=20
+mean=(a+b)/2
+
+print('Mean',mean)
+
+#Task 4
+
+a=25
+b=15
+
+sum_result=a+b
+product_result=a*b
+square_a=a**2
+square_b=b**2
+
+print("Yig'indisi:",sum_result)
+print("Product ko'paytmasi:",product_result)
+print("a ning kvadrati:",square_a)
+print("b ning kvadrati:",square_b)
